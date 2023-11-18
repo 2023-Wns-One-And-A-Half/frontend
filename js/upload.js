a0e2f0a12de08ca5a98d29432d5f1a379d292c64
An-JiYun <agy0512@gmail.com>
2023-11-19 00:11:30 +0900

    업로드페이지 버튼까지 구현

--- js/upload.js
+++ js/upload.js
@@ -18,7 +18,7 @@ function previewImages(event) {
     let files = event.target.files;  // 선택한 파일들
     for (let i = 0; i < files.length; i++) {
         let file = files[i];
-        
+
         if (file.type.match('image.*')) {
             let reader = new FileReader();
             
@@ -33,3 +33,46 @@ function previewImages(event) {
     }
 }
 
+// 업로드 버튼 클릭 시 서버에 데이터 보내기
+let cookie = localStorage.getItem("jsessionid");
+
+let uploadButton = document.getElementById("upload-button");
+uploadButton.addEventListener("click", function () {
+    let name = document.getElementById("product-name").value;
+    let price = document.getElementById("product-price").value;
+    let area = document.getElementById("area").value;
+    let description = document.getElementById("product-description").value;
+    let imageInput = document.getElementById("product-images");
+    let productImages = imageInput.files;
+    console.log(name, price, area, description, productImages);
+    postInfo(name, price, area, description, productImages);
+});
+
+function postInfo(name, price, area, description, productImages){
+    let formData = new FormData();
+    formData.append("name", name);
+    //formData.append("area", area);
+    formData.append("description", description);
+    formData.append("price", price);
+    for (let i = 0; i < productImages.length; i++) {
+        formData.append("productImages", productImages[i]);
+    }
+    console.log(formData);
+
+    fetch(url+"/products", {
+    method: 'POST',
+    headers: {
+        //'Content-Type': 'multipart/form-data',
+        'JSESSIONID' : cookie,
+    },
+    body: formData,
+    })
+    .then(data => {
+        console.log(data);
+        alert("상품이 등록되었습니다.");
+        window.open('./main.html', '_self');
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
